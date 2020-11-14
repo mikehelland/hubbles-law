7b7cd1a3d5005b3ce34acdfea06a6ca0481b80e3
mikehelland <mike@mikehelland.com>
2020-11-14 00:00:49 -0800

    units

--- model.c
+++ model.c
@@ -1,57 +1,60 @@
 #include<stdio.h>
 
-int main()
-{
+
+void runFast() {
     printf("\reticulating splines... fast!\n\n");
 
     unsigned long t = 0;
     double c = 1;
     double H = 0.00035;
 
-    double photon = 0;
-    
-    int hit5 = 0;
-    int hit13 = 0;
+    double photon = 0;    
+    long nextTarget = 200;
 
-    // first, in units of 1 million years per step
-    while (t < 14000) {
+    while (t < 50000) {
         t++;
 
         photon += c / (1 + H * photon);
 
-        if (!hit5 && photon > 5500) {
+        if (photon >= nextTarget) {
             printf("d = %f       t = %lu\n", photon, t);
-            hit5 = 1;
+            nextTarget += 200;
         }
-
-        if (!hit13 && photon > 13800) {
-            printf("d = %f       t = %lu\n", photon, t);
-            hit13 = 1;
-        }
-
-        /*if (t % 500 == 0) {
-            printf("t = %lu     d = %f\n", t, photon);
-        }*/
     }
+}
 
-
+void runSlow() {
     printf("\reticulating splines... slow...\n\n");
 
-    t = 0;
-    photon = 0;
+    // 1 year per step
+    // c = 1 lightyear per year
 
-    // now in units of 1 year per step
-    //        14,00Q00Q000
-    while (t < 14000000000) {
+    unsigned long t = 0;
+    double c = 1;
+    double H = 0.00000000035;
+
+    double photon = 0;    
+    long nextTarget = 1000000000;
+
+    while (1) {
         t++;
 
-        photon += c / (1 - H * photon);
+        photon += c / (1 + H * photon);
 
-            //  50Q00Q000
-        if (t % 500000000 == 0) {
-            printf("t = %lu     d = %f\n", t, photon);
+        if (photon >= nextTarget) {
+            printf("d = %f       t = %lu\n", photon, t);
+            nextTarget += 200000000;
+            return;
         }
     }
 
+}
+
+int main()
+{
+
+    runFast();
+    runSlow();
     return 0;
+
 }
