3a104e6d1603ed8b5c6c9fdff67d970be9abc5d0
mikehelland <mike@mikehelland.com>
2021-04-28 13:26:28 -0700

    embed code

--- presentations/runners/embed.js
+++ presentations/runners/embed.js
@@ -1,76 +1,52 @@
-
-var runner1div = document.getElementById("runner1")
-var runner2div = document.getElementById("runner2")
-var runner3div = document.getElementById("runner3")
-var runner11div = document.getElementById("runner11")
-var runner21div = document.getElementById("runner21")
-var runner31div = document.getElementById("runner31")
-
-
-var H = 0.0000756
 var c = 1
+var H = 0.0000756 // 74 (km/s)/Mpc converted to (ly/y)/Mly
 
 var zoomX = 0.12
-var zoomY = 400
-var offsetX = 80
-var offsetY = 20
-
 var phase2t = 0
-
 
 // the runners 1, 2, and 3 are defined here
 
-var runner1 = setup({color: "green", div: runner1div, next: race => {
-    race.dx = c - H * race.x
-}})
-var runner2 = setup({color: "blue", div: runner2div, flipByTime: true, next: race => {
+var runner1 = setup({color: "blue", div: "runner2", flipByTime: true, next: race => {
     for (race.target of race.targets) {
         if (!race.target.hit) {
             race.target.x += H * race.target.x
         }
     }
     race.dx = c + H * race.x
-}
-})
-var runner3 = setup({color: "red", div: runner3div, next: race => {
+}})
+
+var runner2 = setup({color: "red", div: "runner3", next: race => {
     race.t-- //undo the default time increment 
     race.t += 1 / ( 1 - H * race.x)
 }})
 
-
-// here's the variation
-
-var runner11 = setup({color: "blue", div: runner11div, flipByTime: true, next: race => {
-    for (race.target of race.targets) {
-        if (!race.target.hit) {
-            race.target.x += 1-1/(1 + H * race.target.x)
-        }
-    }
-    race.dx = (1 + H * race.x)
-}
-})
-var runner31 = setup({color: "orange", div: runner31div, next: race => {
-    race.dx = c / (1 + H * race.x)
+var runner3 = setup({color: "green", div: "runner1", next: race => {
+    race.dx = c - H * race.x
 }})
-var runner21 = setup({color: "magenta", div: runner21div, next: race => {
+
+
+// here's the variations
+
+var runner21 = setup({color: "magenta", div: "runner21", next: race => {
     race.t-- //undo the default time increment 
     race.t += 1 + H * race.x
+}})
+
+var runner31 = setup({color: "orange", div: "runner31", next: race => {
+    race.dx = c / (1 + H * race.x)
 }})
 
 
 
 function setup(params) {
     var canvas = document.createElement("canvas")
-    var timer = document.createElement("canvas")
-    params.div.appendChild(timer)
-    params.div.appendChild(canvas)
+    document.getElementById(params.div).appendChild(canvas)
     
-    canvas.width = 840
-    timer.width = 60
+    canvas.width = 900
     canvas.height = 80
-    timer.height = 80
+    var timerWidth = 60
+    var timerHeight = 80
     var ctx = canvas.getContext("2d")
-    var tctx = timer.getContext("2d")
     var race = {
         t: 0,
         dx: c,
@@ -83,7 +59,7 @@ function setup(params) {
         color: params.color
     }
 
-    for (var i = 0; i < (params.targets || 4600); i+=100) {
+    for (var i = 0; i < 4600; i+=100) {
         race.targets.push({start: i, x: i})
     }
 
@@ -91,41 +67,11 @@ function setup(params) {
     var target
 
     var wavelength = 50
+    var frequency = 50
     var nextFlip = wavelength
-    var lastLeft = 0
     var lastZ = 0
 
-    race.draw = () => {
-        ctx.fillStyle = "black"
-        ctx.fillRect(0, 0, canvas.width, canvas.height)
-        tctx.fillStyle = "black"
-        tctx.fillRect(0, 0, timer.width, timer.height)
-
-        ctx.globalAlpha = 0.5 - (phase2t * 0.5)
-        ctx.fillStyle = "white"
-        for (target of race.targets) {
-            ctx.fillRect(target.x * zoomX, 0, 1, canvas.height)
-        }
-        ctx.globalAlpha = 1
-        ctx.fillStyle = params.color
-        ctx.beginPath()
-        for (step of race.steps) {
-            ctx.fillRect((step.x + (phase2t * (step.t - step.x))) * zoomX, step.leftFoot ? 20 : 50, 8, 8)
-        }
-
-        if (race.done) {
-            tctx.lineWidth = 2
-        }
-        tctx.strokeStyle = "white"
-        tctx.beginPath()
-        tctx.moveTo(timer.width / 2, timer.height / 2)
-        tctx.lineTo(timer.width / 2 + Math.sin(race.t/100) * 20, timer.height / 2 + Math.cos(race.t/100) * -20)
-        tctx.stroke()
-
-        tctx.beginPath()
-        tctx.arc(timer.width / 2, timer.height / 2, 22, 0, 2*Math.PI)
-        tctx.stroke()
-    }
+    // here's the main thing
 
     race.next = function () {
         if (this.done) return 
@@ -135,6 +81,7 @@ function setup(params) {
         this.x += this.dx
         params.next(race)
 
+        // ready to step down? then calculate a z
         if (params.flipByTime ? this.t >= nextFlip : this.x >= nextFlip) {
             this.leftFoot = !this.leftFoot
             let step = {leftFoot: this.leftFoot, t: this.t, x: this.x}
@@ -147,7 +94,7 @@ function setup(params) {
                 let lastStep = this.steps[this.steps.length - 1]
                 var f = 1 / (this.t - lastStep.t)
                 step.f = f
-                step.z = (1/wavelength - f) / f 
+                step.z = (1/frequency - f) / f 
             
             }
             lastZ = step.z
@@ -168,6 +115,38 @@ function setup(params) {
         }
     }
 
+
+
+    race.draw = () => {
+        ctx.fillStyle = "black"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+        ctx.globalAlpha = 0.5 
+        ctx.fillStyle = "white"
+        for (target of race.targets) {
+            ctx.fillRect(timerWidth + target.x * zoomX, 0, 1, canvas.height)
+        }
+        ctx.globalAlpha = 1
+        ctx.fillStyle = params.color
+        ctx.beginPath()
+        for (step of race.steps) {
+            ctx.fillRect(timerWidth + step.x * zoomX, step.leftFoot ? 20 : 50, 8, 8)
+        }
+
+        if (race.done) {
+            ctx.lineWidth = 2
+        }
+        ctx.strokeStyle = "white"
+        ctx.beginPath()
+        ctx.moveTo(timerWidth / 2, timerHeight / 2)
+        ctx.lineTo(timerWidth / 2 + Math.sin(race.t/100) * 20, timerHeight / 2 + Math.cos(race.t/100) * -20)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(timerWidth / 2, timerHeight / 2, 22, 0, 2*Math.PI)
+        ctx.stroke()
+    }
+
     return race
 }
 
@@ -176,7 +155,7 @@ runner1.draw()
 runner2.draw()
 runner3.draw()
 
-runner11.draw()
+//runner11.draw()
 runner21.draw()
 runner31.draw()
 
@@ -212,6 +191,7 @@ document.getElementById("start-race-2-button").onclick = e => {
 
     var animateHandle = setInterval(() => {
     
+        //runner11.draw(true)
         runner21.draw(true)
         runner31.draw(true)
     
@@ -228,6 +208,7 @@ document.getElementById("start-race-2-button").onclick = e => {
     var handle = setInterval(() => {
         for (ii = 0; ii < 5; ii++) {
             
+            //runner11.next()
             runner21.next()
             runner31.next()
             
