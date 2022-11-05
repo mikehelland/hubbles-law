6b4023910d814e0b911865a3c7767c58ccb3484b
mikehelland <mike@mikehelland.com>
2022-11-05 02:42:31 -0700

    lcdm

--- python/lookback.py
+++ python/lookback.py
@@ -2,19 +2,24 @@ import numpy
 import cosmolopy.distance as cd
 import json
 
+c = 299792.458
+pc2ly = 3.261564
+H0 = 73
+        
+
 zd = []
 for z in range(1, 21, 1):
-    print -1 * (1.0/(1+z) - 1) * 14
-    zd.append(-1 * (1.0/(1+z) - 1) * 14)
+    print -1 * (1.0/(1+z) - 1) * c/H0 / 1000
+    zd.append(-1 * (1.0/(1+z) - 1) * c/H0 / 1000)
 
 full = []
 #cosmo = {'omega_M_0' : 0.3, 'omega_lambda_0' : 0.7, 'h' : 0.72}
     
-for l0 in numpy.arange(0.68, 0.71, 0.005):
+for l0 in numpy.arange(0.0, 0.1, 0.01):
     print l0
     error = 0
 
-    cosmo = {'omega_M_0' : 0.3, 'omega_lambda_0' : l0, 'h' : 0.72}
+    cosmo = {'omega_M_0' : 0.0, 'omega_lambda_0' : l0, 'h' : 0.73}
     cosmo = cd.set_omega_k_0(cosmo)
     
     data = []
