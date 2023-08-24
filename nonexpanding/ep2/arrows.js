b4be90392909caea34119bf622cdef78495bcbe2
Mike Helland <mike@mikehelland.com>
2023-08-24 16:35:16 -0700

    ep2 drafts

---
+++ nonexpanding/ep2/arrows.js
@@ -0,0 +1,63 @@
+
+(function() {
+
+    var canvas = document.getElementById("arrows")
+    var ctx = canvas.getContext("2d")
+
+    var w = canvas.width
+    var h = canvas.height
+
+    var R = w/2.5
+    var r = 8
+
+    var arrowDepth = 20
+
+    ctx.translate(w/2, h/2)
+
+    for (let i = 0; i < 8; i+=2) {
+        drawRightArrow(ctx, "green", r, R, 0)
+        ctx.rotate(Math.PI / 2)
+    }
+
+    ctx.font = "20pt arial"
+    ctx.fillStyle = "white"
+    ctx.fillText("EXPANSION", 90, 10)
+
+    for (let i = 0; i < 8; i+=2) {
+        drawRightArrow(ctx, "blue", -R + 10, -r - 10, -38)
+        ctx.rotate(Math.PI / 2)
+    }
+
+    ctx.fillStyle = "white"
+    ctx.fillText("GRAVITY", 90, 48)
+
+
+    for (let i = 0; i < 8; i+=2) {
+        drawRightArrow(ctx, "red", r + 50, R - 10, -40)
+        ctx.rotate(Math.PI / 2)
+    }
+
+    ctx.fillStyle = "white"
+    ctx.fillText("DARK ENERGY", 90, -30)
+
+
+
+    function drawRightArrow(ctx, color, x1, x2, y) {
+        ctx.lineWidth = 30
+        ctx.strokeStyle = color
+        ctx.fillStyle = color
+        
+        ctx.beginPath()
+        ctx.moveTo(x1, y)
+        ctx.lineTo(x2 - 6, y)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(x2 - arrowDepth, y - 30)
+        ctx.lineTo(x2 + 10 , y)
+        ctx.lineTo(x2 - arrowDepth, y + 30)
+        ctx.closePath()
+        ctx.fill()
+    }
+
+})()
---
+++ other/lcdm.js
@@ -0,0 +1,131 @@
+var LCDM = (function() {
+    var H0, Omega_L, Omega_M, Omega_k
+
+    function fun(v1, Omega_M, Omega_L, Omega_k) {
+        v2 = v1 * v1
+        v3 = v1 * v1 * v1
+        f = Math.sqrt(Omega_M * v3 + Omega_k * v2 + Omega_L)
+        return 1. / f
+    };
+    
+    function simpson(x0, x2) {
+        h = (x2 - x0) / 2.
+        x1 = x0 + h
+        igr = h * (fun(x0, Omega_M, Omega_L, Omega_k) + 4. * fun(x1, Omega_M, Omega_L, Omega_k) + fun(x2, Omega_M, Omega_L, Omega_k)) / 3.
+        return igr
+    };
+    
+    function sumint(z1) {
+        integrale = 0, zone = 1, deltaz = 0.001
+        while (zone <= (z1 - deltaz)) {
+            zini = zone
+            zfin = zone + deltaz
+            integrale += simpson(zini, zfin);
+            zone += deltaz
+        }
+        return integrale
+    };
+    
+    // -------------------------------------------- 
+    
+    
+    function lookfun(v1, Omega_M, Omega_L, Omega_k) {
+        v2 = v1 * v1
+        v3 = v1 * v1 * v1
+        f = v1 * Math.sqrt(Omega_M * v3 + Omega_k * v2 + Omega_L)
+        return 1. / f
+    };
+    
+    function timeint(z1start, z1end) {
+        s = 0, a = z1start, b = z1end
+        s = 0.5 * (b - a) * (lookfun(a, Omega_M, Omega_L, Omega_k) + lookfun(b, Omega_M, Omega_L, Omega_k))
+        n = 2
+        while (n <= 11) {
+            n2 = n - 2
+            it = Math.pow(2, n2)
+            tnm = it
+            del = (b - a) / tnm
+            x = a + 0.5 * del
+            sum = 0.
+            j = 1
+            while (j <= it) {
+                sum = sum + lookfun(x, Omega_M, Omega_L, Omega_k)
+                x = x + del
+                j += 1
+            }
+            s = 0.5 * (s + (b - a) * sum / tnm)
+            n += 1
+        }
+        return s
+    };
+    
+    // --------------------------------------------
+    
+    function sinh(x) {
+        s = (Math.exp(x) - Math.exp(-x)) / 2.
+        return s
+    };
+    
+    function zToD(z) {
+        // From redshift to distance
+    
+    
+        z1 = 1. + z;
+        h = H0 / 100.;
+        Tnorm = 9.77810945 / h;
+        cvel = 299792.458
+        ch0 = cvel / H0;
+        pig = 3.1415926536;
+        rad = pig / 180.;
+        q0 = Omega_M / 2. - Omega_L;
+        Omega_k = 1. - Omega_M - Omega_L;
+    
+        age = (timeint(1., 50.)) * Tnorm;
+    
+        if (Omega_L > 0. && Omega_k == 0.) {
+            DC = ch0 * sumint(z1);
+            DL = DC * z1
+        }
+        if (Omega_L > 0. && Omega_k < 0.) {
+            curv = Math.sqrt(-Omega_k)
+            r = sumint(z1);
+            DC = ch0 * Math.sin(r * curv) / curv
+            DL = DC * z1
+        }
+        if (Omega_L > 0. && Omega_k > 0.) {
+            curv = Math.sqrt(Omega_k)
+            r = sumint(z1);
+            DC = ch0 * sinh(r * curv) / curv
+            DL = DC * z1
+        }
+        if (Omega_L == 0. && q0 > 0) {
+            q0sq = q0 * q0
+            a = 1. - q0 + q0 * z + (q0 - 1.) * (Math.sqrt(2. * q0 * z + 1.))
+            DL = ch0 * a / q0sq
+            DC = DL / z1
+        }
+        if (Omega_L == 0. && q0 == 0) {
+            DL = ch0 * z * (1. + z / 2.)
+            DC = DL / z1
+        }
+        // Lookback time
+        lookback = timeint(1., z1) * Tnorm
+        // Angular distance
+        DA = DL / (z1 * z1)
+        // Lunghezza in primi corrispondente a 1 Mpc.
+        angle = (60. / rad) * z1 * z1 / DL;
+        // Length in Mpc corresponding to 1 degree on the sky
+        R = rad * DL / (z1 * z1);
+    
+        return {Omega_k, q0, DL, DC, DA, R, angle, lookback, age}
+    }
+    
+
+    return function(H_0, OmegaL, OmegaM, z) {
+
+        H0 = H_0
+        Omega_L = OmegaL
+        Omega_M = OmegaM
+        return zToD(z)
+    }
+})()
